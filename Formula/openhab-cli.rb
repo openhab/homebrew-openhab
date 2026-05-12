b14b65a5c20947fbc2093481e8e499230a3483f2
Florian Hotze <dev@florianhotze.com>
2026-05-12 22:19:51 +0200

    openhab-cli: Release v0.2 as formula
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- Formula/openhab-cli.rb
+++ Formula/openhab-cli.rb
@@ -1,8 +1,8 @@
 class OpenhabCli < Formula
   desc "Homebrewed CLI for openHAB - Empowering the smart home"
   homepage "https://www.openhab.org/"
-  url "https://github.com/openhab/homebrew-openhab/archive/refs/tags/cli-v0.1.tar.gz"
-  sha256 "4318fabe84d4244dc7ad1dab51d85f1723138cb76591acfa452fea6a6227c1e8"
+  url "https://github.com/openhab/homebrew-openhab/archive/refs/tags/cli-v0.2.tar.gz"
+  sha256 "596f6bce7c7ce7e656ec67614ca67dd862af91c1274a6dee032f7ae75fbf5785"
   license "EPL-2.0"
 
   def install
